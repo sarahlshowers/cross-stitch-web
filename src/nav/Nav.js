498d21a21c408defb677b333e361c7eb0de4fd69
SARAH SHOWERS <sshowers@linkedin.com>
2021-09-01 13:10:48 -0400

    coming soon...

---
+++ src/nav/Nav.js
@@ -0,0 +1,9 @@
+function Nav() {
+  return (
+    <nav className="content-container nav">
+      <img src="icons/crossed-stitches-logo.svg" alt="Crossed Stitches icon" />
+    </nav>
+  );
+}
+
+export default Nav;
---
+++ src/table/Table.js
@@ -0,0 +1,20 @@
+function Table() {
+  return (
+    <section className="content-container">
+      {/* <section className="table content-container"></section>
+      <div className="table__header-row">
+        <div className="table__header-row-cell">Number</div>
+        <div className="table__header-row-cell">Color</div>
+      </div>
+      <div className="table__body">
+        <div className="table__row">
+          <div className="table__cell">table cell</div>
+          <div className="table__cell">table cell</div>
+        </div>
+      </div> */}
+      <p>Coming soon...</p>
+    </section>
+  );
+}
+
+export default Table;
